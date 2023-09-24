ab116f10ee2b81f4c0353bc60e35f96864f1d2a4
DerVogel101 <jan.irmer@outlook.de>
2023-09-24 23:20:42 +0200

    reorganized functions into one

--- app.py
+++ app.py
@@ -1,63 +1,60 @@
 from gauss import Gauss
 import flet as ft
-from flet import ThemeMode
+from flet import AppView
 
 
-def main_config(page: ft.Page):
+def main(page: ft.Page):
+    global default_color
     default_color = ft.colors.PRIMARY
+    fields = 3
 
-    def submit_click(e):
+    def conf_submit_click(e):
+        """
+        Deletes elements from the configuration Phase and adds the Elements for the input Phase
+        """
         global fields
-        if input_fields.value == "":
-            input_fields.value = "3"
-        fields = int(input_fields.value)
-        page.window_destroy()
+        if configuration_elements["input_fields"].value == "":
+            configuration_elements["input_fields"].value = "3"
+        fields = int(configuration_elements["input_fields"].value)
 
-    def window_event(e):
-        global fields
-        if e.data == "close":
-            fields = 3
-            page.window_destroy()
+        for element in configuration_elements:
+            page.remove(configuration_elements[element])
 
-    page.window_prevent_close = True
-    page.on_window_event = window_event
+        page.title = "Gleichungssystem Eingabe"
+        input_elements_keys = list(input_elements.keys())
+        for i in range(0, 2):
+            page.add(input_elements[input_elements_keys[i]])
 
-    page.title = "Gleichungssystem"
+        for i in range(fields):
+            page.add(eval(dynamic_elements["entry"]))
 
-    title = ft.Text("Gleichungssystem Konfiguration", size=24)
-    page.add(title)
+        for i in range(2, 4):
+            page.add(input_elements[input_elements_keys[i]])
 
-    # Instructions
-    instructions = ft.Text("Geben Sie die Anzahl der Variablen ein:", size=16)
-    page.add(instructions)
+    def input_clear_click(e):
+        ...
 
-    input_fields = ft.TextField(label="Anzahl", value="3", hint_text="Anzahl der Variablen", border_color=default_color)
-    submit = ft.ElevatedButton(text="Weiter", on_click=submit_click)
-    page.add(input_fields, submit)
-    print(page.controls)
+    def input_submit_click(e):
+        ...
 
+    configuration_elements = {"title": ft.Text("Gleichungssystem Konfiguration", size=24),
+                              "instructions": ft.Text("Geben Sie die Anzahl der Variablen ein:", size=16),
+                              "input_fields": ft.TextField(label="Anzahl", value="3", hint_text="Anzahl der Variablen",
+                                                           border_color=default_color),
+                              "submit": ft.ElevatedButton(text="Weiter", on_click=conf_submit_click)}
 
-def main_window(page: ft.Page):
-    default_color = ft.colors.PRIMARY
+    input_elements = {"title": ft.Text("Gleichungssystem Eingabe", size=24),
+                      "tip": ft.Text("Tipp: Die Einträge müssen wie folgt eingegeben werden:\n1x+2y=6\n2x-1y=2", size=16),
+                      "clear": ft.ElevatedButton(text="Löschen", on_click=input_clear_click),
+                      "submit": ft.ElevatedButton(text="Weiter", on_click=input_submit_click)
+                      }
 
-    page.title = "Gleichungssystem"
+    dynamic_elements = {"entry": 'ft.TextField(label=f"Zeile: {i+1}", border_color=default_color)'}
 
-    # Überschrift
-    title = ft.Text("Gleichungssystem", size=24)
-    page.add(title)
-
-    # Tipp
-    tip = ft.Text("Tipp: Die Einträge müssen wie folgt eingegeben werden:\n"
-                  "1x+2y=6\n"
-                  "2x-1y=2", size=16)
-    page.add(tip)
-
-    # Eingabefelder
-    for i in range(fields):
-        entry = ft.TextField(label=f"Zeile: {i+1}", border_color=default_color)
-        page.add(entry)
+    page.scroll = ft.ScrollMode.AUTO
+    page.title = "Gleichungssystem Konfiguration"
+    for element in configuration_elements:
+        page.add(configuration_elements[element])
 
 
-ft.app(main_config)
-fields: int = fields
-ft.app(target=main_window)
+ft.app(target=main, view=AppView.WEB_BROWSER)
