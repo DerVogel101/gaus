3f2c5782c94a0ece6cf7dde205be37a2c2147dc9
DerVogel101 <jan.irmer@outlook.de>
2023-09-24 17:36:26 +0200

    fixed dark mode issue

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from gauss import Gauss
 import flet as ft
+from flet import ThemeMode
 
 
 def main_config(page: ft.Page):
+    default_color = ft.colors.PRIMARY
+
     def submit_click(e):
         global fields
         if input_fields.value == "":
@@ -28,13 +31,15 @@ def main_config(page: ft.Page):
     instructions = ft.Text("Geben Sie die Anzahl der Variablen ein:", size=16)
     page.add(instructions)
 
-    input_fields = ft.TextField(label="Anzahl", value="3", hint_text="Anzahl der Variablen")
+    input_fields = ft.TextField(label="Anzahl", value="3", hint_text="Anzahl der Variablen", border_color=default_color)
     submit = ft.ElevatedButton(text="Weiter", on_click=submit_click)
     page.add(input_fields, submit)
     print(page.controls)
 
 
 def main_window(page: ft.Page):
+    default_color = ft.colors.PRIMARY
+
     page.title = "Gleichungssystem"
 
     # Überschrift
@@ -49,12 +54,8 @@ def main_window(page: ft.Page):
 
     # Eingabefelder
     for i in range(fields):
-        entry = ft.TextField()
+        entry = ft.TextField(label=f"Zeile: {i+1}", border_color=default_color)
         page.add(entry)
-
-    # Regler
-    scale = ft.Slider(min=1, max=10)
-    page.add(scale)
 
 
 ft.app(main_config)
